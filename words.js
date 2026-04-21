ea94a1f33642cdcacc3c0cfafcc1c076f47c2622
Akshay Rao <raoak@stanford.edu>
2026-04-20 17:53:36 -0700

    change the words cycling

--- words.js
+++ words.js
@@ -1,14 +1,14 @@
-export var words1 = ['Orchestrate', 'Design', 'Operate', 'Plan', 'Site'];
+export var words1 = ['Design', 'Operate', 'Plan', 'Site'];
 
 export var words2 = [
   'desalination', 'chlor-alkali', 'green hydrogen', 'water treatment',
-  'caustic soda', 'brine electrolysis', 'aluminum smelting',
-  'copper refining', 'lithium refining', 'semiconductor fabrication', 'battery recycling', 'waste-to-energy',
+  'caustic soda', 'brine electrolysis', 'aluminum smelting', 'transmission network',
+  'lithium refining', 'battery recycling', 'waste-to-energy',
   'biogas generation', 'carbon capture', 'paper pulping', 'textile dyeing',
-  'dairy processing', 'sugar refining', 'beer brewing', 'juice processing', 'starch processing', 'chemical manufacturing', 'soda ash',
+  'dairy processing', 'sugar refining', 'beer brewing', 'juice processing', 
+  'starch processing', 'chemical manufacturing', 'soda ash',
   'sodium hypochlorite', 'oxygen generation', 'nitrogen generation', 'ozone generation',
-  'electroplating', 'fuel cell', 'electrolysis', 'hydrogen peroxide',
-  'seawater electrolysis', 'CO₂ electrolysis', 'hydropower', 'nanofiltration', 'ultrafiltration', 'microfiltration',
-  'water reuse', 'potable reuse', 'agricultural reuse', 'industrial waste', 'wastewater reuse', 'greywater reuse', 'blackwater reuse',
-  'electrochemical reforming', 'electrochemical synthesis', 'electrochemical storage', 
+  'hydrogen peroxide', 'seawater electrolysis', 'CO₂ electrolysis', 'hydropower', 
+  'nanofiltration', 'ultrafiltration', 'microfiltration','water reuse', 'groundwater recharge', 
+  'agricultural reuse', 'industrial waste', 'wastewater reuse','electrochemical synthesis'
 ];
